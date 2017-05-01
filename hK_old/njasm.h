bcee299c9047eb6645db9f408f4302a74b0d571f
hKpoGo <hendrik.kegel@mni.thm.de>
2017-05-01 14:52:40 +0200

    added my old files FYI

---
+++ hK_old/njasm.h
@@ -0,0 +1,17 @@
+
+#ifndef NJ_ASSEMBLER_HEADER
+#define NJ_ASSEMBLER_HEADER
+
+#define HALT 0
+#define PUSHC 1
+#define ADD 2
+#define SUB 3
+#define MUL 4
+#define DIV 5
+#define MOD 6
+#define RDINT 7
+#define WRINT 8
+#define RDCHAR 9
+#define WRCHR 10
+
+#endif
---
+++ hK_old/njvm.h
@@ -0,0 +1,10 @@
+/* Version number */
+#define NJVM_VERSION 1
+
+/* Constant argument strings */
+#define ARGUMENT_VERSION "--version"
+#define ARGUMENT_HELP "--help"
+
+/* fucntions */
+
+void printHelp(void);
