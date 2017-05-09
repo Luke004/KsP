f5a1a66ad30c6b0d285e54292a1283b6c727c527
hKpoGo <hendrik.kegel@mni.thm.de>
2017-05-09 13:01:53 +0200

    removed prog 1

--- njvm.c
+++ njvm.c
@@ -8,20 +8,6 @@
 int stack [1000];
 int PC = 0;
 int IR = 0;
-unsigned int program1 [] = {
-    (PUSHC << 24)|IMMEDIATE(3),
-    (PUSHC << 24)|IMMEDIATE(4),
-    (ADD << 24),
-    (PUSHC << 24)|IMMEDIATE(10),
-    (PUSHC << 24)|IMMEDIATE(6),
-    (SUB << 24),
-    (MUL << 24),
-    (WRINT << 24),
-    (PUSHC << 24)|IMMEDIATE(10),
-    (WRCHR << 24),
-    (HALT << 24)
-};
-
 
 int main(int argc, char *argv [])
 {
