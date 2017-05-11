64f2f24bfd2bc37b962d330e852c16a3c0fd945a
Lukas <lukas.hilfrich@mni.thm.de>
2017-05-11 15:57:25 +0200

    added defines for new instructions

--- njvm.h
+++ njvm.h
@@ -20,6 +20,12 @@
 #define WRINT 8
 #define RDCHR 9
 #define WRCHR 10
+#define PUSHG 11
+#define POPG 12
+#define ASF 13
+#define RSF 14
+#define PUSHL 15
+#define POPL 16
 
 #define IMMEDIATE(x) ((x) & 0x00FFFFFF)
 #define SIGN_EXTEND(i) ((i) & 0x00800000 ? (i) | 0xFF000000 : (i))
